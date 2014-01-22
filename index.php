8637d961f1241d2655f9f4b5e85ed4c86dada706
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-01-22 00:22:00 -0500

    Initialized website

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+ <head>
+  <title>MSU WIC</title>
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="img/favicon.png">
+  <!-- CSS -->
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,700|Playfair+Display:700|Codystar:300' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/main.css" media="screen">  
+  </head>
+
+ <body>
+ 	<header>
+ 		<div class="icon-frame"><img class="icon" src="img/icon.png" alt="Women in Computing logo"></div>
+ 		<div class="WIC"><span class="tag">&lt;</span><span class="highlight">W</span><span class="mobile-hide">omen </span><span class="highlight">I</span><span class="mobile-hide">n </span><span class="highlight">C</span><span class="mobile-hide">omputing</span> <span class="tag">/&gt;</span></div>
+ 	</header>
+
+ 	<div class="core">
+ 		<div class="event-highlight">
+ 			<h1>Next Event</h1>
+ 			<p>Pour-over yr keytar, letterpress leggings tattooed literally fanny pack deep v Tonx cliche. Distillery banjo Brooklyn Helvetica High Life, Tonx chillwave single-origin coffee fingerstache.</p>
+ 			<div class="well">5:00 PM <i>//</i> 3105 EB <i>//</i> Tuesday, January 28th, 2014</div>
+ 		</div>
+
+ 		<h1>What is Women in Computing?</h1>
+ 		<p>MSU Women in Computing (MSU WIC) is an organization of students and faculty (both men and women) who work to recruit, support and retain women in computing fields. If you are interested in computing, we would love to see you at our next meeting.</p>
+ 		<p>We host many events during the semester, including tech talks from <a href="#">visiting companies</a> (every other Tuesday), <a href="#">hackathons</a>, <a href="#">Girl Scout workshops</a>, <a href="#">web development workshops</a>, and more. Check out our <a href="#">calendar</a> to keep up to date.</p>
+
+ 		<h1>Recent News</h1>
+ 		<div class="news-update">
+ 			<h2>Technology Workshop!</h2>
+ 			<p>Do you want your students to learn how to design and create their OWN website? Invite them to the MSU Women in Computing (WIC) workshop, and we'll teach them some basic tools to help them make websites. We'll also be teaching some basic <a href="http://scratch.mit.edu/">Scratch</a> programming.<br><br>
+
+			<b>Who:</b> Any middle to high school age girl interested in Computer Science! Although this workshop is geared toward older students, we also welcome younger students.<br><br>
+
+			<b>When:</b> Saturday, November 16th from 10:00 AM-2:00 PM. Please bring your own lunch.<br><br>
+
+			<b>Where:</b> Room 3105 Engineering Building, Michigan State University. Parking is available in visitor lot 39 on <a href="http://maps.msu.edu/interactive/">this map</a>.<br><br>
+
+			Important: Please have interested students <a href="https://skydrive.live.com/redir?page=survey&resid=985037F5DBFED447!190&authkey=!AA6woJ9YYk9ssVg">RSVP here</a>, also fill out <a href="http://cse.msu.edu/msuwic/forms/MSU-Consent-Form.doc">this form</a> and make sure they bring it with them the day of the workshop.</p>
+ 		</div>
+ 		<div class="news-update">
+ 			<h2>Technology Workshop!</h2>
+ 			<p>Do you want your students to learn how to design and create their OWN website? Invite them to the MSU Women in Computing (WIC) workshop, and we'll teach them some basic tools to help them make websites. We'll also be teaching some basic <a href="http://scratch.mit.edu/">Scratch</a> programming.<br><br>
+
+			<b>Who:</b> Any middle to high school age girl interested in Computer Science! Although this workshop is geared toward older students, we also welcome younger students.<br><br>
+
+			<b>When:</b> Saturday, November 16th from 10:00 AM-2:00 PM. Please bring your own lunch.<br><br>
+
+			<b>Where:</b> Room 3105 Engineering Building, Michigan State University. Parking is available in visitor lot 39 on <a href="http://maps.msu.edu/interactive/">this map</a>.<br><br>
+
+			Important: Please have interested students <a href="https://skydrive.live.com/redir?page=survey&resid=985037F5DBFED447!190&authkey=!AA6woJ9YYk9ssVg">RSVP here</a>, also fill out <a href="http://cse.msu.edu/msuwic/forms/MSU-Consent-Form.doc">this form</a> and make sure they bring it with them the day of the workshop.</p>
+ 		</div>
+ 		<div class="news-update">
+ 			<h2>Technology Workshop!</h2>
+ 			<p>Do you want your students to learn how to design and create their OWN website? Invite them to the MSU Women in Computing (WIC) workshop, and we'll teach them some basic tools to help them make websites. We'll also be teaching some basic <a href="http://scratch.mit.edu/">Scratch</a> programming.<br><br>
+
+			<b>Who:</b> Any middle to high school age girl interested in Computer Science! Although this workshop is geared toward older students, we also welcome younger students.<br><br>
+
+			<b>When:</b> Saturday, November 16th from 10:00 AM-2:00 PM. Please bring your own lunch.<br><br>
+
+			<b>Where:</b> Room 3105 Engineering Building, Michigan State University. Parking is available in visitor lot 39 on <a href="http://maps.msu.edu/interactive/">this map</a>.<br><br>
+
+			Important: Please have interested students <a href="https://skydrive.live.com/redir?page=survey&resid=985037F5DBFED447!190&authkey=!AA6woJ9YYk9ssVg">RSVP here</a>, also fill out <a href="http://cse.msu.edu/msuwic/forms/MSU-Consent-Form.doc">this form</a> and make sure they bring it with them the day of the workshop.</p>
+ 		</div>
+ 	</div>
+ </body>
+
+</html>
